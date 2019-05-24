0c8d9055c77db39a01810829ee5508c73245d333
赵建凯 <01115004@wisedu.com>
2019-05-24 17:21:07 +0800

    Hide output from expect

--- expect/core.ex
+++ expect/core.ex
@@ -4,6 +4,7 @@ set port [lindex $argv 1]
 set username [lindex $argv 2]
 set password [lindex $argv 3]
 set timeout -1
+#log_user 0 #Hide output from expect,https://stackoverflow.com/questions/14601526/hide-output-from-expect
 spawn ssh -p $port $username@$ip
 expect {
     "password" {send "$password\r";}
